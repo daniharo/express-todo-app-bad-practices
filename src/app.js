526c1fbfd4cf29bd1e4f2ad9ea22c155470556bb
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-04-28 20:04:16 +0200

    Change CommonJS sintax to modules

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 // === imports == //
-express = require('express')
-bodyParser = require('body-parser')
+import express from "express"
+import bodyParser from "body-parser"
 
 
 // === initialisation == //
-app = express()
+const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
